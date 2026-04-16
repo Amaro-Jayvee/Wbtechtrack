df85d834fb921fbadd73c33ac007854944c6d9f4
John Jayvee T. Amaro <cc.johnjayvee.amaro@cvsu.edu.ph>
2026-04-16 08:47:42 +0800

    FIX: Serve Django static/admin assets in production

--- backend/djangomonitor/djangomonitor/urls.py
+++ backend/djangomonitor/djangomonitor/urls.py
@@ -16,14 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.shortcuts import render
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 if settings.DEBUG:
