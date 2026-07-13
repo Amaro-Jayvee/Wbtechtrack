c9f27a52281ba4dc9631be73e16b7cb05f30f363
John Jayvee T. Amaro <cc.johnjayvee.amaro@cvsu.edu.ph>
2026-07-14 07:16:32 +0800

    Add backend health root route

--- backend/djangomonitor/djangomonitor/urls.py
+++ backend/djangomonitor/djangomonitor/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,6 +24,7 @@ from django.views.static import serve
 from app.views import *
 
 urlpatterns = [
+    path('', lambda request: JsonResponse({'status': 'ok', 'service': 'techtrack-backend'})),
     path('admin/', admin.site.urls),
     path('app/', include('app.urls')),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
